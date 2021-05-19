e2045aa888ea667b0eac239aabb4fd4b48f5052c
Justin Francos <justinfrancos@gmail.com>
2021-05-19 14:35:22 -0400

    setup template dir

--- crancos.sh
+++ crancos.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-# create-tailwind-snowpack-react-app
+# CRA a la Francos
+
+# If we're running locally, $BASE is CWD:
+BASE="$(dirname "$0")"
+
+# If we're running via npx, $BASE
+# is $CWD/../lib/node_modules/[package name]:
+if [ ! -f "$BASE"/package.json ]; then
+  BASE="$(dirname "$BASE")"/lib/node_modules/"$(basename "$0")"
+fi
+
+# Get absolute path
+BASE="$(cd "$BASE" && pwd)"
+
+echo "$BASE"
+ls "$BASE"
 
 TEMPLATE=app-template-react
 APP=App.jsx
@@ -22,40 +37,6 @@ npm install react-icons
 echo "module.exports.plugins = module.exports.plugins.concat(['@snowpack/plugin-postcss', '@jadex/snowpack-plugin-tailwindcss-jit'])
 module.exports.devOptions.port = 3000" >>snowpack.config.js
 
-# Add minimal postcss and tailwind config files
-echo "module.exports = {
-    plugins: {
-      tailwindcss: {},
-    },
-  };" >postcss.config.js
-
-echo "module.exports = {
-    mode: 'jit',
-    purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
-  };" >tailwind.config.js
-
-# Setup css file to incorporate tailwind + set default div display
-echo "@tailwind base;
-@tailwind components;
-@tailwind utilities;
-
-html,
-body,
-#root {
-  height: 100%;
-}
-
-.grid-overlay {
-  grid-row-start: 1;
-  grid-column-start: 1;
-}
-
-div {
-  display: flex;
-
-}
-" >src/index.css
-
 # Replace sample code with a minimal, full-screen app
 rm src/App.css src/logo.svg
 echo "import React, { useState, useEffect, useRef } from 'react';
@@ -77,69 +58,10 @@ function App() {
 export default App;
 " >src/"$APP"
 
-mkdir .vscode
-echo '{
-  "Add handler with event param": {
-    "prefix": "h",
-    "scope": "javascriptreact",
-    "body": [
-      "const handle$1 = (${2:{ target: { value \\} \\}}) => {",
-      "  $0",
-      "}"
-    ],
-    "description": "Add handler with event param"
-  },
-  "Add mapped handler with event param": {
-    "prefix": "hi",
-    "scope": "javascriptreact",
-    "body": [
-      "const handle$1 = (index) => (${2:{ target: { value \\} \\}}) => {",
-      "  $0",
-      "}"
-    ],
-    "description": "Add mapped handler with event param"
-  },
-  "Add a div w/ className": {
-    "prefix": "d",
-    "scope": "javascriptreact",
-    "body": ["<div className=\"$1\">", "  $2", "</div>"],
-    "description": "Add a div w/ className"
-  },
-  "Use state": {
-    "prefix": "s",
-    "scope": "javascriptreact",
-    "body": [
-      "const [${1:state}, set${1/(.*)/${1:/capitalize}/}] = useState(${2:null})"
-    ],
-    "description": "Use state"
-  },
-  "Update at index": {
-    "prefix": "ui",
-    "scope": "javascriptreact",
-    "body": [
-      "set${1/(.*)/${1:/capitalize}/}($1 => [",
-      "  ...$1.slice(0, index),",
-      "  { ...$1[index], $2},",
-      "  ...$1.slice(index + 1),",
-      "]);"
-    ],
-    "description": "Update at index"
-  },
-  "Add event listener": {
-    "scope": "javascriptreact",
-    "prefix": "el",
-    "body": [
-      "useEffect(() => {",
-      "  const handle$1 = ({$3}) => {",
-      "    $0",
-      "  };",
-      "  document.addEventListener('$2', handle$1);",
-	  "  return () => document.removeEventListener('$2', handle$1);",
-	  "});"
-    ],
-    "description": "Add event listener"
-  }
-}' >.vscode/crancos.code-snippets
+# Add minimal postcss and tailwind config files
+# Setup css file to incorporate tailwind + set default div display
+# Copy snippets file
+cp -r "$BASE"/template/. .
 
 # update git repo
 git add -A
---
+++ template/tailwind.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mode: 'jit',
+    purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
+  };
