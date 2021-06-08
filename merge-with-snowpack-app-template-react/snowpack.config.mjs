5294961d4a5c22814d05c42946509d98fd90be21
Justin Francos <justinfrancos@gmail.com>
2021-06-08 18:35:47 -0400

    setup snowpack local hostname

--- merge-with-snowpack-app-template-react/snowpack.config.mjs
+++ merge-with-snowpack-app-template-react/snowpack.config.mjs
@@ -6,5 +6,6 @@ config.plugins = config.plugins.concat([
 ]);
 config.devOptions.port = 3000;
 config.devOptions.open = "none";
+config.devOptions.hostname = "dev.localhost"
 config.env = { MAGIC_PUBLISHABLE_KEY: "" };
 export default config;
