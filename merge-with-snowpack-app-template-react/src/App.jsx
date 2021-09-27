a5c3b42777c7060fefe0321ff64598e25007dfaa
Justin Francos <justinfrancos@gmail.com>
2021-09-27 14:47:32 -0400

    fixed async issue for safari

--- merge-with-snowpack-app-template-react/src/App.jsx
+++ merge-with-snowpack-app-template-react/src/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Auth } from './Auth';
-import Controller from './Controller';
+import React from "react";
+import { Auth } from "./Auth";
+import Controller from "./Controller";
 
 const { MODE } = __SNOWPACK_ENV__;
 
@@ -12,16 +12,18 @@ console.log({ MODE });
 // not actually unregistering in dev since
 // MODE still shows up as production
 
-if ('serviceWorker' in navigator && MODE === 'production') {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js');
+if ("serviceWorker" in navigator && MODE === "production") {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js");
   });
 }
 
-if ('serviceWorker' in navigator && MODE === 'development') {
-  const registrations = await navigator.serviceWorker.getRegistrations();
-  console.log(registrations);
-  registrations.forEach((registration) => registration.unregister());
+if ("serviceWorker" in navigator && MODE === "development") {
+  (async () => {
+    const registrations = await navigator.serviceWorker.getRegistrations();
+    console.log(registrations);
+    registrations.forEach((registration) => registration.unregister());
+  })();
 }
 
 function App() {
